3435a8bae52c517e15b82c704d04d9926c5f7845
Adam Tull <adamchristoph18@gmail.com>
2023-05-16 19:15:33 -0400

    Removed imports of loginPage and signupPage from app since I deleted those components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -18,12 +16,6 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
         </Switch>
       )}
     </>
