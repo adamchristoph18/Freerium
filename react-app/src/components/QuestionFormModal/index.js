dde5a4dca4cd8a2dbc5f49a85e2da048dd209278
Adam Tull <adamchristoph18@gmail.com>
2023-05-17 18:18:48 -0400

    updated styling to question form and added space select field

--- react-app/src/components/QuestionFormModal/index.js
+++ react-app/src/components/QuestionFormModal/index.js
@@ -9,16 +9,40 @@ function QuestionFormModal({ type, title }) {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
     const [questionTitle, setQuestionTitle] = useState("");
+    const [space, setSpace] = useState("");
     const [context, setContext] = useState("");
     const [imageUrl, setImageUrl] = useState("");
 
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
 
+    const spaces = ['Technology', 'Exercise', 'Personal Health', 'Financial Well-Being', 'Travel', 'Career Goals', 'Miscellaneous'];
+
+    const spacesObj = {
+        'Technology': 1,
+        'Exercise': 2,
+        'Personal Health': 3,
+        'Financial Well-Being': 4,
+        'Travel': 5,
+        'Career Goals': 6,
+        'Miscellaneous': 7
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let data;
+        if (type === "create") {
+            data = await dispatch(addNewQuestionThunk({
+
+            }))
+        }
+    }
+
     return (
         <>
             <h2 className="modal-title">{title}</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="modal-input-label">
                     Title
                     <input
@@ -42,6 +66,23 @@ function QuestionFormModal({ type, title }) {
                     />
                 </label>
                 <label className="modal-input-label">
+                    Select a Space
+                    <select
+                    className="space-select"
+                    value={space}
+                    onChange={(e) => setSpace(e.target.value)}
+                    >
+                    {spaces.map(space => (
+                        <option
+                        key={space}
+                        value={space}
+                        >
+                        {space}
+                        </option>
+                    ))}
+                    </select>
+                </label>
+                <label className="modal-input-label">
                     Image URL (Optional)
                     <input
                         type="text"
@@ -52,7 +93,7 @@ function QuestionFormModal({ type, title }) {
                         onChange={(e) => setQuestionTitle(e.target.value)}
                     />
                 </label>
-                <button className="clickable submit-question site-color-b" type="submit">Submit Question</button>
+                <button className="clickable submit-question site-color-b" type="submit">Add Question</button>
             </form>
         </>
     )
