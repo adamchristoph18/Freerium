40de3631a77da9e8d843dee3f21ab0d37bbfbf38
Adam Tull <adamchristoph18@gmail.com>
2023-05-17 19:35:05 -0400

    updated styling for errors on question form modal

--- react-app/src/components/QuestionFormModal/index.js
+++ react-app/src/components/QuestionFormModal/index.js
@@ -58,9 +58,13 @@ function QuestionFormModal({ type, title }) {
         <>
             <h2 className="modal-title">{title}</h2>
             <form onSubmit={handleSubmit}>
-                {Object.values(errors).length > 0 && (
-                    <ErrorHandler errors={errors} />
-                )}
+                <div className='modal-errors'>
+                        {Object.values(errors).map(error => (
+                            <p key={error} className='error-p'>
+                                {error}
+                            </p>
+                        ))}
+                    </div>
                 <label className="modal-input-label">
                     Title
                     <input
