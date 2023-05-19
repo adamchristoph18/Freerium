5c38c30362c4d5f34fd91bcea48c15371070d169
Adam Tull <adamchristoph18@gmail.com>
2023-05-19 13:36:39 -0400

    have individual question card rendering on question show page

--- react-app/src/components/QuestionShow/index.js
+++ react-app/src/components/QuestionShow/index.js
@@ -1,9 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { displayQuestionThunk } from '../../store/questions';
+import QuestionCard from '../QuestionCard';
 import "./QuestionShow.css";
 
 
 function QuestionShow() {
+    const { questionId } = useParams();
+    const questionObject = useSelector(state => state.questions.singleQuestion);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(displayQuestionThunk(questionId));
+    }, dispatch, questionId);
+
+    if (!questionObject) return null;
+
     return (
         <>
+            <QuestionCard question={questionObject} />
             <p>Hello world</p>
         </>
     )
