579b937e5a8cb1e464715bc920f55ef3dfd4a548
Adam Tull <adamchristoph18@gmail.com>
2023-05-20 10:00:06 -0400

    finished delete answer functionality

--- react-app/src/components/ConfirmDeleteAnswerModalButton/index.js
+++ react-app/src/components/ConfirmDeleteAnswerModalButton/index.js
@@ -10,16 +10,19 @@ function ConfirmDeleteAnswerModalButton({ answer, questionId }) {
     const history = useHistory();
     const dispatch = useDispatch();
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await dispatch(deleteAnswerThunk(answer.id));
+        await dispatch(displayQuestionThunk(questionId));
+        closeModal();
+    }
+
     return (
         <>
             <h2 className="modal-title are-u-sure-delete">Delete your answer?</h2>
             <div
                 className="yes-confirm clickable site-color-b"
-                onClick={() => {
-                    dispatch(deleteAnswerThunk(answer.id));
-                    dispatch(displayQuestionThunk(questionId));
-                    closeModal();
-                }}
+                onClick={handleSubmit}
             >
                 Yes please!
             </div>
