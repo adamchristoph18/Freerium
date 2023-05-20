53893fd110472d573c9937c2ca9a7549f7c3cd4a
Adam Tull <adamchristoph18@gmail.com>
2023-05-20 11:17:06 -0400

    finished all crud functionality for answers

--- react-app/src/components/AnswerCard/index.js
+++ react-app/src/components/AnswerCard/index.js
@@ -18,7 +18,7 @@ function AnswerCard({ answer, questionId }) {
                 Answer published by {userWroteAnswer() ? "you" : author.first_name}
             </div>
             <div className="answer-time site-color">
-                @ {answer.created_at}
+                @ {answer.created_at > answer.updated_at ? answer.created_at : answer.updated_at}
             </div>
             <div className="main-answer-text">
                 {answer.body}
