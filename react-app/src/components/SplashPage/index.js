b2a62aa8547be3a8f7558cc887de2e214d7a8a21
Adam Tull <adamchristoph18@gmail.com>
2023-05-22 10:42:23 -0400

    added welcome to Freerium line to left side of splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -2,9 +2,14 @@ import "./SplashPage.css";
 
 function SplashPage() {
     return (
-        <>
-            Hello world
-        </>
+        <div className="splash-page">
+            <div className="splash-page-left">
+                <p className="welcome-to">Welcome to</p>
+                <h1 className="site-color freerium-splash-page">Freerium</h1>
+            </div>
+            <div className="splash-page-right">
+            </div>
+        </div>
     )
 }
 
