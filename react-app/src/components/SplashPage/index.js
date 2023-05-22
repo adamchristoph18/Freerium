5cd4e99b160493662097689d8ecdb5f655e67242
Adam Tull <adamchristoph18@gmail.com>
2023-05-22 11:10:45 -0400

    finished basic styling for initial splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -7,9 +7,11 @@ function SplashPage() {
     return (
         <div className="splash-page">
             <div className="splash-page-left">
-                <p className="welcome-to">Welcome to</p>
-                <h1 className="site-color freerium-splash-page">Freerium</h1>
-                <p className="site-tag-line">A wellness site for questions, answers, knowledge, and freedom.</p>
+                <div className="left-side-content">
+                    <p className="welcome-to">Welcome to</p>
+                    <h1 className="site-color freerium-splash-page">Freerium</h1>
+                    <p className="site-tag-line">A wellness site for questions, answers, knowledge, and freedom.</p>
+                </div>
             </div>
             <div className="splash-page-right">
                 <OpenModalButton
