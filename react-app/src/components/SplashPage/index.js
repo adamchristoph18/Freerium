c77a28a5c091698a42a71aa1bd5f7a53fdc9e8cb
Adam Tull <adamchristoph18@gmail.com>
2023-05-22 10:47:43 -0400

    added tag line for site on splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -6,6 +6,7 @@ function SplashPage() {
             <div className="splash-page-left">
                 <p className="welcome-to">Welcome to</p>
                 <h1 className="site-color freerium-splash-page">Freerium</h1>
+                <p className="site-tag-line">A wellness site for questions, answers, knowledge, and freedom.</p>
             </div>
             <div className="splash-page-right">
             </div>
