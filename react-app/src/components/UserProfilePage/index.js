94e908ab31f06ced3777fc58041dd45b6a1a7fa8
Adam Tull <adamchristoph18@gmail.com>
2023-05-22 17:53:27 -0400

    finished basic styling for top section of user profile component

--- react-app/src/components/UserProfilePage/index.js
+++ react-app/src/components/UserProfilePage/index.js
@@ -1,9 +1,21 @@
 import "./UserProfilePage.css";
 
 function UserProfilePage({ user }) {
+    console.log("tisssss ------------> ", user);
+
     return (
         <div>
-            Hello world
+            <div className="user-profile-top">
+                <img className="user-img-profile-page" src={user.profile_image_url} alt="" />
+                <div className="user-info-card">
+                    <h3 className="your">Your...</h3>
+                    <p className="user-detail">Name: {user.first_name} {user.last_name}</p>
+                    <p className="user-detail">Username: {user.username}</p>
+                    <p className="user-detail">Account was last updated: {user.created_at}</p>
+                    <p className="user-detail">Number of questions: </p>
+                    <p className="user-detail">Number of answers: </p>
+                </div>
+            </div>
         </div>
     )
 }
