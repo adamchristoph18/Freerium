ee4813aa56643f281e8ac1e061e002e0ab886491
Adam Tull <adamchristoph18@gmail.com>
2023-05-23 17:18:48 -0400

    added github icon to footer with external link

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,7 +5,10 @@ function Footer() {
         <div className="footer-container">
             <div className="name-plus-linkedin">
                 <p className="my-name">Adam Tull</p>
-                <a href="https://www.linkedin.com/in/adam-tull-a54207a6/" target="_blank"><i class="linkedin-icon fa-brands fa-linkedin fa-beat-fade clickable" /></a>
+                <a href="https://www.linkedin.com/in/adam-tull-a54207a6/" target="_blank">
+                    <i class="brand-icon fa-brands fa-linkedin clickable" /></a>
+                <a href="https://github.com/adamchristoph18" target="_blank">
+                    <i class="brand-icon fa-brands fa-github clickable" /></a>
             </div>
             <p className="site-year">@2023</p>
         </div>
