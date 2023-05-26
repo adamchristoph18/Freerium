b6810eaa4594ded104fb58f5d2b263d55f4bc4f2
Adam Tull <adamchristoph18@gmail.com>
2023-05-26 15:35:27 -0400

    finished basic 404 page

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,9 +1,11 @@
+import NothingToShow from "../NothingToShow";
 import "./PageNotFound.css";
 
 function PageNotFound() {
     return (
         <div className="page-not-found">
-            You must be lost
+            <p className="oops">Oops! Page Not Found</p>
+            <NothingToShow />
         </div>
     )
 }
