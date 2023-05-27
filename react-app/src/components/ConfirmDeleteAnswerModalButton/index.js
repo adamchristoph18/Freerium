bbc290f22271e68189461d004930848202eaae07
Adam Tull <adamchristoph18@gmail.com>
2023-05-27 13:42:10 -0400

    added the puff loader loading icon to the delete answer modal

--- react-app/src/components/ConfirmDeleteAnswerModalButton/index.js
+++ react-app/src/components/ConfirmDeleteAnswerModalButton/index.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
 import { deleteAnswerThunk } from "../../store/answers";
 import { displayQuestionThunk } from '../../store/questions';
+import PuffLoader from "react-spinners/PuffLoader";
 import "./ConfirmDeleteAnswerModalButton.css";
 
 function ConfirmDeleteAnswerModalButton({ answer, questionId }) {
+	const [loading, setLoading] = useState(false);
     const { closeModal } = useModal();
     const dispatch = useDispatch();
 
+    const override = {
+        display: "absolute",
+        margin: "0 auto",
+        borderColor: "red",
+        bottom: "150px",
+        left: "80px"
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
+
         await dispatch(deleteAnswerThunk(answer.id));
         await dispatch(displayQuestionThunk(questionId));
         closeModal();
@@ -27,6 +40,12 @@ function ConfirmDeleteAnswerModalButton({ answer, questionId }) {
             <div className="no-forget-it clickable site-color-b" onClick={closeModal}>
                 No, thanks
             </div>
+            <PuffLoader
+                loading={loading}
+                color="#36d7b7"
+                cssOverride={override}
+                size={150}
+                />
         </>
     )
 }
