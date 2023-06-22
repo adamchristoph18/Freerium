e9898cb7bf17d71d89513d30470ef9bd8a13dab5
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 16:57:02 -0400

    Added conditional css classes of active-space for when a user clicks on a space

--- react-app/src/components/SpacesContainer/index.js
+++ react-app/src/components/SpacesContainer/index.js
@@ -1,17 +1,19 @@
 import { useHistory } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import "./SpacesContainer.css";
 
 
 function SpacesContainer({ spaces }) {
+    const { spaceId } = useParams();
     const history = useHistory();
 
     return (
         <div className="spaces-container">
             <h3 className="spaces-title site-color">Spaces/Categories</h3>
-            <span className="space-tile clickable" onClick={() => history.push('/')}>All Questions</span>
+            <span className={!spaceId ? "space-tile clickable space-active" : "space-tile clickable"} onClick={() => history.push('/')}>All Questions</span>
             {spaces.map(space => (
                 <span
-                    className="space-tile clickable"
+                    className={parseInt(spaceId) === space.id ? "space-tile clickable space-active" : "space-tile clickable"}
                     title={space.description}
                     key={space.id}
                     onClick={() => history.push(`/spaces/${space.id}/all-questions`)}
