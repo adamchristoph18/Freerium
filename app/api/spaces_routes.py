f9a01677ef08bdef18834507e377d9e77ff01a89
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 14:08:59 -0400

    Added a spaces route that queries for all of the questions of a particular space/category

--- app/api/spaces_routes.py
+++ app/api/spaces_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required
-from app.models import User, Space
+from app.models import User, Space, Question
 
 spaces_routes = Blueprint('spaces', __name__)
 
@@ -22,3 +22,11 @@ def all_spaces():
     """
     spaces = Space.query.all()
     return { 'spaces': [space.to_dict() for space in spaces] }
+
+@spaces_routes.route('/<int:id>/questions')
+def all_questions_for_individual_space(id):
+    """
+    Route to get all questions of a particular space
+    """
+    questions = Question.query.where(Question.space_id == id)
+    return { 'questions': [question.to_dict() for question in questions] }
