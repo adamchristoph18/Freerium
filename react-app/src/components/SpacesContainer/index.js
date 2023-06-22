f3ff3f7dff2141d6f44be67ff2120b84ced32cef
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 15:47:14 -0400

    Added a history.push to redirect the user if they click on a space to go to that spaces questions

--- react-app/src/components/SpacesContainer/index.js
+++ react-app/src/components/SpacesContainer/index.js
@@ -1,12 +1,20 @@
+import { useHistory } from "react-router-dom";
 import "./SpacesContainer.css";
 
 
 function SpacesContainer({ spaces }) {
+    const history = useHistory();
+
     return (
         <div className="spaces-container">
             <h3 className="spaces-title site-color">Spaces/Categories</h3>
             {spaces.map(space => (
-                <span className="space-tile clickable" title={space.description} key={space.id}>{space.name}</span>
+                <span
+                    className="space-tile clickable"
+                    title={space.description}
+                    key={space.id}
+                    onClick={() => history.push(`/spaces/${space.id}/all-questions`)}
+                    >{space.name}</span>
             ))}
         </div>
     )
