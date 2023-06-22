35996f4e32b99b8a42e0b72eac05ae4b29a0d5b7
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 13:57:22 -0400

    Finished basic styling for the spaces container component on the left side of the homepage

--- react-app/src/components/SpacesContainer/index.js
+++ react-app/src/components/SpacesContainer/index.js
@@ -4,7 +4,7 @@ import "./SpacesContainer.css";
 function SpacesContainer({ spaces }) {
     return (
         <div className="spaces-container">
-            <h3 className="spaces-title">Spaces</h3>
+            <h3 className="spaces-title site-color">Spaces/Categories</h3>
             {spaces.map(space => (
                 <span className="space-tile clickable" title={space.description} key={space.id}>{space.name}</span>
             ))}
