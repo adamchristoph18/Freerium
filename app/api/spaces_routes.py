690a55ddff5451bff4e09beaf4fba33f75dd622c
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 12:36:41 -0400

    Wrote first endpoint for the spaces to get all of the spaces

--- app/api/spaces_routes.py
+++ app/api/spaces_routes.py
@@ -3,3 +3,22 @@ from flask_login import login_required
 from app.models import User, Space
 
 spaces_routes = Blueprint('spaces', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+@spaces_routes.route('/all')
+def all_spaces():
+    """
+    GET all spaces
+    """
+    spaces = Space.query.all()
+    return { 'spaces': [space.to_dict() for space in spaces] }
