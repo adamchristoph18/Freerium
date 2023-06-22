9a4ee4f9548b635fb84f2ab1e5dc6ba2d9d35f3e
Adam Tull <adamchristoph18@gmail.com>
2023-06-22 13:44:18 -0400

    Updated the position attribute of the spaces container on the homepage to fixed

--- react-app/src/components/SpacesContainer/index.js
+++ react-app/src/components/SpacesContainer/index.js
@@ -4,8 +4,9 @@ import "./SpacesContainer.css";
 function SpacesContainer({ spaces }) {
     return (
         <div className="spaces-container">
+            <h3 className="spaces-title">Spaces</h3>
             {spaces.map(space => (
-                <span className="space-tile" key={space.id}>{space.name}</span>
+                <span className="space-tile clickable" title={space.description} key={space.id}>{space.name}</span>
             ))}
         </div>
     )
